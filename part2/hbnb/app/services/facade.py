ad7153721d2b5e0a3a62ad240a3898983936f8d1
Callacos <callacos74@gmail.com>
2025-03-01 17:26:04 +0100

    Refactor HBnBFacade methods for improved readability and consistency

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -4,6 +4,7 @@ from app.models.amenity import Amenity
 from app.models.place import Place
 from app.models.review import Review
 
+
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
@@ -22,16 +23,16 @@ class HBnBFacade:
         existing_user = self.get_user_by_email(user_data['email'])
         if existing_user:
             raise ValueError("Email already registered")
-        
+
         # Create and validate new user
         user = User(**user_data)
         user.checking()
         self.user_repo.add(user)
         return user
-    
+
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
-    
+
     def get_user_by_email(self, email):
         """Get user by email"""
         return next(
@@ -41,7 +42,7 @@ class HBnBFacade:
 
     def get_all_users(self):
         return self.user_repo.get_all()
-    
+
     def update_user(self, user_id, user_data):
         user = User(**user_data)
         user.checking()
@@ -69,7 +70,6 @@ class HBnBFacade:
         return amenity
 
     # REVIEW METHODS
-
     def create_review(self, review_data):
         """Create a new review"""
         # Create and validate new user
@@ -84,17 +84,16 @@ class HBnBFacade:
     def get_all_reviews(self):
         return self.review_repo.get_all()
 
-
     def get_reviews_by_place(self, place_id):
         """Get all reviews for a specific place"""
         # First check if place exists
         place = self.get_place(place_id)
         if not place:
             return None
-            
+
         # Return all reviews for this place
         return [
-            review for review in self.review_repo.get_all() 
+            review for review in self.review_repo.get_all()
             if review.place_id == place_id
         ]
 
@@ -111,14 +110,14 @@ class HBnBFacade:
 
     def create_place(self, place_data):
         amenities = place_data.pop('amenities', [])
-        
+
         # Create and validate place
         place = Place(**place_data)
         place.checking()
-        
+
         # Add amenities back to place
         place.amenities = amenities
-        
+
         # Save place to repository
         self.place_repo.add(place)
         return place
@@ -126,26 +125,26 @@ class HBnBFacade:
     def get_place(self, place_id):
         return self.place_repo.get(place_id)
 
-
     def get_all_places(self):
         return self.place_repo.get_all()
 
     def update_place(self, place_id, place_data):
         amenities = place_data.pop('amenities', [])
-        
+
         # Create and validate place
         place = Place(**place_data)
         place.checking()
-        
+
         # Add amenities back to place
         place.amenities = amenities
-        
+
         # Save place to repository
         self.place_repo.update(place_id, place_data)
         return place
-    
+
     def get_place_by_title(self, title):
         return next(
-            (place for place in self.place_repo.get_all() if place.title == title),
+            (place for place in self.place_repo.get_all()
+                if place.title == title),
             None
         )
